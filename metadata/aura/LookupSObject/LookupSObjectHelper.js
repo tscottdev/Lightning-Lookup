493570f6be556befc0e97537f36e02e013513f01
Tony Scott <github@anthonyscott.com>
2015-10-31 15:19:54 +0000

    Version 2 : Uses an aura:iteration to render the lookup list items.

--- metadata/aura/LookupSObject/LookupSObjectHelper.js
+++ metadata/aura/LookupSObject/LookupSObjectHelper.js
@@ -10,14 +10,12 @@
      */
     doSearch : function(cmp) {
         // Get the search string, input element and the selection container
-        var searchString = cmp.get("v.searchString");
+        var searchString = cmp.get('v.searchString');
         var inputElement = cmp.find('lookup');
-        var lookupList = cmp.find("lookuplist");
-        var lookupListItems = cmp.find("lookuplist-items");
+        var lookupList = cmp.find('lookuplist');
 
         // Clear any errors and destroy the old lookup items container
         inputElement.set('v.errors', null);
-        lookupListItems.set('v.body', new Array());
         
         // We need at least 2 characters for an effective search
         if (typeof searchString === 'undefined' || searchString.length < 2)
@@ -34,7 +32,7 @@
         var sObjectAPIName = cmp.get('v.sObjectAPIName');
 
         // Create an Apex action
-        var action = cmp.get("c.lookup");
+        var action = cmp.get('c.lookup');
 
         // Mark the action as abortable, this is to prevent multiple events from the keyup executing
         action.setAbortable();
@@ -52,14 +50,15 @@
                 // Get the search matches
                 var matches = response.getReturnValue();
 
-                // If we have no matches, return
+                // If we have no matches, return nothing
                 if (matches.length == 0)
                 {
+                    cmp.set('v.matches', null);
                     return;
                 }
                 
-                // Render the results
-                this.renderLookupComponents(cmp, lookupListItems, matches);
+                // Store the results
+                cmp.set('v.matches', matches);
             }
             else if (state === "ERROR") // Handle any error by reporting it
             {
@@ -84,103 +83,14 @@
     },
 
     /**
-     * Render the Lookup List Components
-     */    
-    renderLookupComponents : function(cmp, lookupListItems, matches)
-    {
-        // list Icon SVG Path and Class
-        var listIconSVGPath = cmp.get('v.listIconSVGPath');
-        var listIconClass = cmp.get('v.listIconClass');
-
-        // Array of components to create
-        var newComponents = new Array();
-         
-        // Add a set of components for each match found
-        for (var i=0; i<matches.length; i++)
-        {
-            // li element
-            newComponents.push(["aura:html", {
-                "tag" : "li",
-                "HTMLAttributes" : {
-                    "class" : "slds-lookup__item" 
-                }
-            }]);
-
-            // a element
-            newComponents.push(["aura:html", {
-                "tag" : "a",
-                "HTMLAttributes" : { 
-                    "id" : cmp.getGlobalId() + '_id_' + matches[i].SObjectId, 
-                    "role" : "option", 
-                    "onclick" : cmp.getReference("c.select") 
-                }
-            }]);
-
-            // svg component
-            newComponents.push(["c:svg", {
-                "class" : "slds-icon " + listIconClass + " slds-icon--small",
-                "xlinkHref" : listIconSVGPath
-            }]);
-
-            // output text component
-            // For some reason adding an aura:id to this component failed to record the id for subsequent cmp.find requests
-            newComponents.push(["ui:outputText", {
-                "value" : matches[i].SObjectLabel
-            }]);
-        }
-
-        // Create the components
-        $A.createComponents(newComponents, function(components, status) {
-            // Creation succeeded
-            if (status === "SUCCESS")
-            {
-                // Get the List Component Body
-                var lookupListItemsBody = lookupListItems.get('v.body');
-
-                // Iterate the created components in groups of 4, correctly parent them and add them to the list body
-                for (var i=0; i<components.length; i+=4)
-                {
-                    // Identify the releated components
-                    var li = components[i];
-                    var a = components[i+1];
-                    var svg = components[i+2];
-                    var outputText = components[i+3];
-
-                    // Add the <a> to the <li>
-                    var liBody = li.get('v.body');
-                    liBody.push(a);
-                    li.set('v.body', liBody);
-
-                    // Add the <svg> and <outputText> to the <a>
-                    var aBody = a.get('v.body');
-                    aBody.push(svg);
-                    aBody.push(outputText);
-                    a.set('v.body', aBody);
-
-                    // Add the <li> to the container
-                    lookupListItemsBody.push(li);
-                }
-
-                // Update the list body
-                lookupListItems.set('v.body', lookupListItemsBody);
-           }
-           else // Report any error
-           {
-                this.displayToast('Error', 'Failed to create list components.');
-           }
-        });
-
-    },
-
-    /**
      * Handle the Selection of an Item
      */
     handleSelection : function(cmp, event) {
         // Resolve the Object Id from the events Element Id (this will be the <a> tag)
         var objectId = this.resolveId(event.currentTarget.id);
 
-        // The Object label is the 2nd child (index 1)
-        var objectLabel = event.currentTarget.children[1].innerText;
+        // The Object label is the inner text)
+        var objectLabel = event.currentTarget.innerText;
 
         // Log the Object Id and Label to the console
         console.log('objectId=' + objectId);
